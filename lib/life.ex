384e3e67682d0cff445d349b2045dfc3e02d419c
Jason Desrosiers <jdesrosi@gmail.com>
2016-12-18 21:59:51 -0800

    Rule 2: Survival
    Any live cell with two or three live neighbors lives on to the next
    generation

--- lib/life.ex
+++ lib/life.ex
@@ -1,3 +1,4 @@
 defmodule Life do
   def next_gen(:live, neighbors) when neighbors < 2, do: :dead # underpopulation
+  def next_gen(:live, neighbors) when neighbors == 2 or neighbors == 3, do: :live # survival
 end
--- spec/life_spec.exs
+++ spec/life_spec.exs
@@ -7,4 +7,11 @@ defmodule LifeSpec do
       expect Life.next_gen(:live, 1) |> should(eq :dead)
     end
   end
+
+  describe "Any live cell with two or three live neighbors" do
+    it "should live on to the next generation" do
+      expect Life.next_gen(:live, 2) |> should(eq :live)
+      expect Life.next_gen(:live, 3) |> should(eq :live)
+    end
+  end
 end
