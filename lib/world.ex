29d8662b1e4a70b9e9bdeabe6149fc098e009759
Jason Desrosiers <jdesrosi@gmail.com>
2016-12-19 12:53:16 -0800

    Add a live cell to the world
    I stopped at an intermediate step.  To go further, I will need a method
    of getting the location of the neighbors of a cell.  I'll do that next
    and come back to finish this part.

--- lib/world.ex
+++ lib/world.ex
@@ -1,5 +1,6 @@
 defmodule World do
   def empty, do: %{}
-  def state(_world, _point), do: :dead
+  def state(world, point), do: Map.get(world, point, :dead)
   def neighbors(_world, _point), do: 0
+  def add(world, point), do: put_in world[point], :live
 end
--- spec/world_spec.exs
+++ spec/world_spec.exs
@@ -10,4 +10,21 @@ defmodule WorldSpec do
       expect World.neighbors(world, %Point{x: 346546, y: 6546590}) |> should(eq 0)
     end
   end
+
+  describe "Adding a live cell to an empty world" do
+    let :point, do: %Point{x: 0, y: 0}
+    let :world, do: World.empty |> World.add(point)
+
+    it "should make that cell live with no neighbors" do
+      expect World.state(world, point) |> should(eq :live)
+      expect World.neighbors(world, point) |> should(eq 0)
+    end
+
+    xit "should increment the neighbor count for the cell above it" do
+      neighbor = Point.north_of(point)
+
+      expect World.state(world, neighbor) |> should(eq :dead)
+      expect World.neighbors(world, neighbor) |> should(eq 1)
+    end
+  end
 end
