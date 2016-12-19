2dfc946f2c14298b65dbb67fddca13fb95fc8c60
Jason Desrosiers <jdesrosi@gmail.com>
2016-12-18 22:09:51 -0800

    Rule 3: Overpopulation
    Any live cell with more than three live neighbors dies, as if by
    overpopulation.

--- lib/life.ex
+++ lib/life.ex
@@ -1,4 +1,5 @@
 defmodule Life do
   def next_gen(:live, neighbors) when neighbors < 2, do: :dead # underpopulation
   def next_gen(:live, neighbors) when neighbors == 2 or neighbors == 3, do: :live # survival
+  def next_gen(:live, neighbors) when neighbors > 3, do: :dead # overpopulation
 end
--- spec/life_spec.exs
+++ spec/life_spec.exs
@@ -2,7 +2,7 @@ defmodule LifeSpec do
   use ESpec
 
   describe "Any live cell with fewer than two live neighbors" do
-    it "should die in the next generation" do
+    it "should be dead in the next generation" do
       expect Life.next_gen(:live, 0) |> should(eq :dead)
       expect Life.next_gen(:live, 1) |> should(eq :dead)
     end
@@ -12,6 +12,13 @@ defmodule LifeSpec do
     it "should live on to the next generation" do
       expect Life.next_gen(:live, 2) |> should(eq :live)
       expect Life.next_gen(:live, 3) |> should(eq :live)
+    end
+  end
+
+  describe "Any live cell with more than three live neighbors" do
+    it "should be dead in the next generation" do
+      expect Life.next_gen(:live, 4) |> should(eq :dead)
+      expect Life.next_gen(:live, 8) |> should(eq :dead)
     end
   end
 end
